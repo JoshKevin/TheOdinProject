99d62704f9d4c02f5a30f78e8f3814d0206a06f4
JoshKevin <joshkevinserdan@gmail.com>
2023-08-01 14:31:25 +0800

    finish Math 2 in MDN article

--- foundations/javascript-basics/fundamentals-part-1/javascript.js
+++ foundations/javascript-basics/fundamentals-part-1/javascript.js
@@ -1,10 +1,24 @@
-let a = 10,
-    b = 14,
-    c = 4,
-    d = 2;
-let e = a + b;
-let f = c - d;
-let finalResult = e * f;
-console.log(finalResult);
-let evenOddResult = finalResult % 2;
-console.log(evenOddResult);
+// let a = 10,
+//     b = 14,
+//     c = 4,
+//     d = 2;
+// let e = a + b;
+// let f = c - d;
+// let finalResult = e * f;
+// console.log(finalResult);
+// let evenOddResult = finalResult % 2;
+// console.log(evenOddResult);
+
+//MATH 2
+// Final result should be 4633.33
+// Add/update your code here
+let result = 7 + 13 / 9 + 7;
+let result2 = 100 / 2 * 6;
+console.log(result);
+result *= result2;
+console.log(result);
+let finalResult = result.toFixed(2);
+console.log(typeof(finalResult))
+let finalNumber = ++finalResult;
+console.log(finalNumber)
+console.log(typeof(finalNumber))
