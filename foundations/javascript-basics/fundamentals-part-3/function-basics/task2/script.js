cf200093b8b9d063c3feba3d67a258ff929bd918
JoshKevin <joshkevinserdan@gmail.com>
2023-08-07 16:47:05 +0800

    finish task2

---
+++ foundations/javascript-basics/fundamentals-part-3/function-basics/task2/script.js
@@ -0,0 +1,19 @@
+//Write a function min(a,b) which returns the least of two numbers a and b.
+//For instance:
+//min(2, 5) == 2
+//min(3, -1) == -1
+//min(1, 1) == 1
+
+// function leastNum(a,b) {
+//     if (a < b) {
+//         return a;
+//     } else {
+//         return b;
+//     }
+// };
+
+//ternary operator
+// function leastNUm(a,b) {
+//     return (a < b) ? a : b;
+// }
+
