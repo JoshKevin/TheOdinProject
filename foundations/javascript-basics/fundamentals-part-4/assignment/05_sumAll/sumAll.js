0413847f7aa8478d5ea1fb6c2a75f33c53eb7dd4
JoshKevin <joshkevinserdan@gmail.com>
2023-08-20 01:10:02 +0800

    finish sum All exercise

--- foundations/javascript-basics/fundamentals-part-4/assignment/05_sumAll/sumAll.js
+++ foundations/javascript-basics/fundamentals-part-4/assignment/05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+function sumAll(a, b) {
+    result = 0;
+    if (a < 0 || b < 0) return "ERROR"
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    if (a > b) {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+    for (let i = a; i <= b; i++) {
+        // console.log(i);
+        result = result + i;
+        // console.log(result)
 
-};
-
-// Do not edit below this line
-module.exports = sumAll;
+    }
+    return result;
+}
