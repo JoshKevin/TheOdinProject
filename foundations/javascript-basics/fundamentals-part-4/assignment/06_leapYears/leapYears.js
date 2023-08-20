477fd45134bb0a450be7ca241ccc70e2a0e4e96e
JoshKevin <joshkevinserdan@gmail.com>
2023-08-20 13:19:13 +0800

    finish leap year exercise

--- foundations/javascript-basics/fundamentals-part-4/assignment/06_leapYears/leapYears.js
+++ foundations/javascript-basics/fundamentals-part-4/assignment/06_leapYears/leapYears.js
@@ -1,6 +1,7 @@
-const leapYears = function() {
-
-};
-
-// Do not edit below this line
-module.exports = leapYears;
+function leapYears(year) {
+    if (year % 4 == 0 && (year % 100 !== 0 || year % 400 === 0)) {
+        return true;
+    } else {
+         return false
+    }
+}
