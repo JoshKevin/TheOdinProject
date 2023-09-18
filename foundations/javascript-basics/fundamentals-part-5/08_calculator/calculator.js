733dc3e9b9309508975239eaf011195561f166c0
JoshKevin <joshkevinserdan@gmail.com>
2023-09-18 13:52:21 +0800

    add calculator starter file

---
+++ foundations/javascript-basics/fundamentals-part-5/08_calculator/calculator.js
@@ -0,0 +1,27 @@
+const add = function(a, b) {
+    return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b
+};
+
+const sum = function(array) {
+	let total = array.reduce((a, b) => {
+        return a + b;
+    }, 0);
+};
+
+console.log(sum(3, 5, 6, 10))
+const multiply = function() {
+
+};
+
+const power = function() {
+	
+};
+
+const factorial = function() {
+	
+};
+
