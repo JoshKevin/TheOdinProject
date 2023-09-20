fd36137fd6c26a8ccdea3ec5e99a2d8110ec80b8
JoshKevin <joshkevinserdan@gmail.com>
2023-09-21 01:22:10 +0800

    finish calculator exercise

--- foundations/javascript-basics/fundamentals-part-5/08_calculator/calculator.js
+++ foundations/javascript-basics/fundamentals-part-5/08_calculator/calculator.js
@@ -10,18 +10,30 @@ const sum = function(array) {
 	let total = array.reduce((a, b) => {
         return a + b;
     }, 0);
+    return total
+};
+// console.log(sum([3,4,7,7]));
+
+const multiply = function(array) {
+    let total = array.reduce((a, b) => {
+        return a * b;
+    }, 1);
+    return total;
+};
+// console.log(multiply([5,5,5,7]));
+
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-console.log(sum(3, 5, 6, 10))
-const multiply = function() {
-
+const factorial = function(n) {
+	if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
-const power = function() {
-	
-};
 
-const factorial = function() {
-	
-};
-
+console.log(factorial(8));
