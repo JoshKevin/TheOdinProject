9930ef2046ce43ca694170602f709f5522e44b23
JoshKevin <joshkevinserdan@gmail.com>
2023-09-21 21:39:31 +0800

    finish palindromes exercise

--- foundations/javascript-basics/fundamentals-part-5/09_palindromes/palindromes.js
+++ foundations/javascript-basics/fundamentals-part-5/09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let array = word.toLowerCase().split("")
+    let reverse = array.reverse().join('');
+    console.log(reverse);
+    if (word === reverse) {
+        console.log('True');
+        return true;
+    }
 };
 
-// Do not edit below this line
-module.exports = palindromes;
+palindromes('level')
