ef10d5986b1409d466086fb08fb101d055ca3c9a
JoshKevin <joshkevinserdan@gmail.com>
2023-09-28 02:34:51 +0800

    finish getTheTitles

---
+++ foundations/javascript-basics/fundamentals-part-5/11_getTheTitles/getTheTitles.js
@@ -0,0 +1,19 @@
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+const getTheTitles = books.map( book => {
+    return book.title
+})
+
+console.log(getTheTitles);
+
+// Do not edit below this line
+
