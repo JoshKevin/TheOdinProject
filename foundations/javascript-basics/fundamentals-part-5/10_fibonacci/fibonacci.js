b96e74582962aa7c68e348acb96ed51b710c1498
JoshKevin <joshkevinserdan@gmail.com>
2023-09-28 01:49:34 +0800

    finish fibonacci exercise

--- foundations/javascript-basics/fundamentals-part-5/10_fibonacci/fibonacci.js
+++ foundations/javascript-basics/fundamentals-part-5/10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return 'number cannot be lower than 0'
+    } 
+    if (num === 0) {
+        return 0
+    }
+
+    let a = 1;
+    let b = 0;
+    
+    for (let i = 2; i <= num; i++) {
+        let current = a + b;
+        b = a;
+        a  = current
+    }
+    
+    return a;
 
 };
-
-// Do not edit below this line
-module.exports = fibonacci;
+console.log(fibonacci(7));
