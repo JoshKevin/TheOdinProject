523a60833fe4197ea8161354b4ab0bd85aca74d7
JoshKevin <joshkevinserdan@gmail.com>
2023-10-01 03:02:46 +0800

    starter files filter exercises

---
+++ foundations/javascript-basics/fundamentals-part-5/addExercises/filterExercises/main.js
@@ -0,0 +1,8 @@
+function fiveAndGreaterOnly(arr) {
+    let filtered = arr.filter(num => {
+        return arr > 5;
+    })
+    retu
+  }
+  // test
+  console.log(fiveAndGreaterOnly([3, 6, 8, 2]));
